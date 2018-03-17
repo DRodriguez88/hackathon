8f4c1310ebb2b310d3f201fba53468f5baa1c7e4
Dj <rodriguezdanilo88@gmail.com>
2018-03-16 21:55:59 -0700

    findCheapest function needs work

--- src/App.js
+++ src/App.js
@@ -10,12 +10,14 @@ class App extends Component {
       latitude : 'none',
       longitude : 'none',
       distance : '5',
-      stations : []
+      stations : [],
+      cheapest: '',
     }
     this.getLocation = this.getLocation.bind(this)
     this.getData = this.getData.bind(this)
     this.showPosition = this.showPosition.bind(this)
     this.changeHandler = this.changeHandler.bind(this)
+    this.findCheapest = this.findCheapest.bind(this)
   }
 
   changeHandler(e) {
@@ -30,7 +32,7 @@ class App extends Component {
     this.setState({
       latitude : latitude,
       longitude : longitude,
-    },()=>console.log(this.state),alert('Location Received'))
+    },alert('Location Received'))
     }
   
   getLocation() {
@@ -46,10 +48,21 @@ class App extends Component {
       const data = res.data;
       this.setState({
         stations: this.state.stations.concat(data.stations)
-      }, ()=>console.log(this.state))
+      }, ()=>this.findCheapest())
       })
   }
 
+  findCheapest() {
+    var lowest = this.state.stations[0];
+    // for(let i=0; i<this.state.stations.length-1; i++){
+    //   if(this.state.stations[i] < this.state.stations[i+1].reg_price){
+    //     lowest = this.state.stations[i]
+    //   }
+    // }
+    this.setState({
+      cheapest: lowest
+    }, ()=>console.log(this.state))
+  }
 
 
   render() {
@@ -58,24 +71,55 @@ class App extends Component {
         <h1>Gassed Up</h1>
         <h5>Find the gas stations</h5>
         <hr />
-          <div className='card col-5'>
-            <div className='card-body'>
-              <div className='row pb-2'>
-                <h5 className='ml-2 mr-2'>
-                  First:
-                </h5>
-                <a className='btn btn-warning' onClick={this.getLocation}>Get Your Location</a>
+          <div className='row'>
+            <div className='card col-5 ml-3'>
+              <div className='card-body'>
+                <div className='row pb-2'>
+                  <h5 className='ml-2 mr-2'>
+                    First:
+                  </h5>
+                  <a className='btn btn-warning' onClick={this.getLocation}>Get Your Location</a>
+                  <span>(May take a sec)</span>
+                </div>
+                <div className='row pb-2'>
+                  <h5 className='ml-2 mr-2'>Distance to search:</h5>
+                  <input  name='distance' placeholder='miles(1-50)' onChange={this.changeHandler} type='number'></input>
+                </div>
+                <a className='btn btn-success btn-block' onClick={this.getData}>Find stations!</a>
               </div>
-              <div className='row pb-2'>
-                <h5 className='ml-2 mr-2'>Distance to search:</h5>
-                <input  name='distance' placeholder='miles(1-50)' onChange={this.changeHandler} type='number'></input>
-              </div>
-              <a className='btn btn-success btn-block' onClick={this.getData}>Find stations!</a>
             </div>
+            
+            {
+              (this.state.stations.length > 0)?
+                <div className='card col-6 ml-4'>
+                  <div className='card-header'>
+                    Cheapest Station for Regular Unleaded
+                  </div>
+                  <div className='card-body'>
+                    <div className='row'>
+                      <div className='card'>
+                        <p>The cheapest gas within {this.state.distance} miles:</p>
+                        <p className='font-weight-bold'>{this.state.cheapest.address}</p>
+                        <p className='font-weight-bold'>{this.state.cheapest.city}, {this.state.cheapest.region}, {this.state.cheapest.zip}</p>
+                      </div>
+                      <div className='card ml-4'>
+                        <h3>Regular Unleaded</h3>
+                        <h1 className='ml-5 font-weight-bold'>${this.state.cheapest.reg_price}</h1>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              :
+              <div className='card col-6 ml-4'>
+                <h3 className='mt-3'>Provide your location and a radius to search.</h3>
+                <h4 className='mt-3'>The default search radius is 5 miles.</h4>
+              </div>
+            }
+
           </div>
-          <div name='mapDiv'>
-            {<MapContainer stations={this.state.stations} lat={this.state.latitude} lng={this.state.longitude}/>}
-          </div>
+
+        {<MapContainer stations={this.state.stations} lat={this.state.latitude} lng={this.state.longitude}/>}
+        
       </div>
     );
   }
--- src/MapContainer.js
+++ src/MapContainer.js
@@ -1,20 +1,23 @@
 import React, { Component } from 'react';
 import './App.css';
 import {Map, Marker, GoogleApiWrapper} from 'google-maps-react';
-import MarkerMaker from './MarkerMaker.js';
  
 export class MapContainer extends Component {
+  mapZoom = 10;
+  style = {
+    height: '60%',
+    width: '80%'
+  }
 
   render() {
       return (
-        <Map google={this.props.google} zoom={2}>
-          {/* { this.props.stations.map((station, index) => 
-              <MarkerMaker google={this.props.google} 
-                key={index} 
-                lat={parseFloat(station.lat)} 
-                lng={parseFloat(station.lng)}/>
-              )} */}
-
+        <Map 
+          google={this.props.google} 
+          zoom={this.mapZoom}
+          style={this.style} 
+          center={{
+            lat: 32.7090788, 
+            lng: -117.15620969999999}}>
           { 
             this.props.stations.map((station, index) => 
               <Marker 
@@ -25,9 +28,10 @@ export class MapContainer extends Component {
                   lng: parseFloat(station.lng)
                 }}
               />
-          )}    
+            )
+          }    
           
-          <Marker id='homeMarker' google={this.props.google} position={{lat: [this.props.lat], lng: [this.props.lng]}}/>
+        <Marker id='homeMarker' google={this.props.google} position={{lat: [this.props.lat], lng: [this.props.lng]}}/>
         </Map>
       );
     }
